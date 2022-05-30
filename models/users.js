52fe65635370e15d22f58b08295c92fd711d7a5b
Hitesh Gurnani <55234408+Hitesh-gurnani@users.noreply.github.com>
2022-05-30 16:19:23 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
-
 var User = new Schema({
     firstname: {
         type: String,
